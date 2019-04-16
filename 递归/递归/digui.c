77d45e502234b44faeabf8824744efba646785a9
Youtong Ren <1092828478@qq.com>
2019-04-16 22:24:32 +0800

    递归小练习

---
+++ 递归/递归/digui.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//输入1234 输出1 2 3 4
+//void Print(int x) {
+//	if (x > 9) {
+//		Print(x / 10);
+//	}
+//	printf("%d ", x % 10);
+//}
+//
+//
+//int main() {
+//	int num = 1234;
+//	Print(num);
+//
+//	system("pause");
+//	return 0;
+//}
+
+//不创建临时变量 求字符串长度
+
+//int array(int arr[]) {
+//	if ()
+//}
+//
+//int main() {
+//	char a[] = "abcd";
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+//求 n! 
+
+//int step(int x) {
+//	if (x <= 1) {
+//		return 1;
+//	}
+//	return x*step(x- 1);
+//}
+//
+//int main() {
+//	printf("%d\n", step(4));
+//	system("pause");
+//	return 0;
+//}
